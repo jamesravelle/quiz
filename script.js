6b6c489e5b62f314b23f34f9719f472f043f6a52
jxrvll <jxrvll@gmail.com>
2020-06-14 21:56:30 -0400

    Added methods for getting questions and answers

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ var startButton = document.querySelector('#start-button');
 var quizFrame = document.querySelector('.quiz-frame');
 var highScore = document.querySelector('#highscore');
 var scoreboard = document.querySelector('.scoreboard');
+var result = document.querySelector('.result');
 var questionText = quizFrame.querySelectorAll('h1')[0]
 var introP = quizFrame.querySelectorAll('p')[0]
 var answerText = quizFrame.querySelectorAll('div')[0]
 
+var time = 60;
 // Create objects with questions
 var quizObj = {
     question1: {
@@ -59,13 +61,58 @@ var quizObj = {
             "Skinbark"
         ],
         correct : "Treebeard"
+    },
+    getQuestion : function(x){
+        var question = "";
+        switch(x){
+            case 1:
+                question = quizObj.question1.question;
+                break;
+            case 2:
+                question = quizObj.question2.question;
+                break;
+            case 3:
+                question = quizObj.question3.question;
+                break;
+            case 4:
+                question = quizObj.question4.question;
+                break;
+            case 5:
+                question = quizObj.question5.question;
+                break;
+        }
+        return question;
+    },
+    getAnswers : function(a, b){
+        var answers = "";
+        switch(a){
+            case 1:
+                answers = quizObj.question1.answers[b];
+                break;
+            case 2:
+                answers = quizObj.question2.answers[b];
+                break;
+            case 3:
+                answers = quizObj.question3.answers[b];
+                break;
+            case 4:
+                answers = quizObj.question4.answers[b];
+                break;
+            case 5:
+                answers = quizObj.question5.answers[b];
+                break;
+        }
+        return answers;
     }
-}
+};
 
+console.log(quizObj.getAnswers(1,2));
 function startTimer(){
-    scoreboard.style.display = "flex";
-    var time = 10;
+    // Show scoreboard div
+    scoreboard.style.opacity = "1";
+    // Output time
     timerEl.textContent = time;
+    // Countdown time
         interval = setInterval(function() {
             if (time > 0) {
                 time--
@@ -83,7 +130,7 @@ function setQuestion(){
     startButton.style.display = "none";
 
     // Set header to question
-    questionText.textContent = quizObj.question1.question;
+    questionText.textContent = quizObj.getQuestion(1);
 
     // Create answer options
     for(var i = 0; i < quizObj.question1.answers.length; i++){
@@ -98,18 +145,42 @@ function setQuestion(){
     var answerOption = document.querySelector('.answer-option')
 }
 
+function correct(){
+    if(result.style.opacity = "0"){
+        result.style.opacity = "1";
+    }
+    result.textContent = "Correct :)";
+    result.style.backgroundColor = "#33FF5E";
+}
+
+function wrong(){
+    if(result.style.opacity = "0"){
+        result.style.opacity = "1";
+    }
+    result.textContent = "Wrong :(";
+    result.style.backgroundColor = "#FF5333";
+    time -= 10;
+    timerEl.textContent = time;
+}
+
 // Start quiz
 startButton.addEventListener("click", function(){
     event.preventDefault();
-    // startTimer();
+    // Show scoreboard and result
+    
+    
+    startTimer();
     setQuestion();
 })
 
 quizFrame.addEventListener("click", function(e){
     event.preventDefault();
     // Test if the clicked answer matches the correct answer
+    // should equal any correct answer list after &&
     if (e.target.classList.value.indexOf('answer-option') > 0 && e.target.innerHTML === quizObj.question1.correct){
-        alert("correct");
+        correct();
+    } else if (e.target.classList.value.indexOf('answer-option') > 0){
+        wrong();
     }
 })
 
