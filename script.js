21d6b1ba9495470c8974dcf478e4f112a630239c
jxrvll <jxrvll@gmail.com>
2020-06-15 20:52:57 -0400

    Finalize quiz, added scoreboard and progress bar

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 // Set variables
-var timerEl = document.querySelector('#timer');
+var timer = document.querySelector('#timer');
 var startButton = document.querySelector('#start-button');
+var progressBar = document.querySelector('#progressBar');
 var quizFrame = document.querySelector('.quiz-frame');
 var highScore = document.querySelector('#highscore');
 var scoreboard = document.querySelector('.scoreboard');
 var result = document.querySelector('.result');
 var score = document.querySelector('#score');
 var endScreen = document.querySelector('.end');
-
 
 // These are the text fields in the quiz fields
 var questionText = quizFrame.querySelectorAll('h1')[0]
@@ -18,6 +18,7 @@ var answerText = quizFrame.querySelectorAll('div')[0]
 var questionPosition = 1;
 var time = 60;
 var scoreValue = 0;
+
 // Create objects with questions
 var quizObj = {
     question1: {
@@ -135,21 +136,29 @@ var quizObj = {
     }
 };
 
-console.log(quizObj.getCorrectAnswers(2))
+// Check if there is data in local storage for the score object, if not create an empty one
+var scoreObj = JSON.parse(localStorage.getItem('scoreObj'));
+if (scoreObj === null){
+    var scoreObj = {
+        user : [],
+        score : []
+    }
+}  
 
 function startTimer(){
     // Show scoreboard div
     scoreboard.style.opacity = "1";
     // Output time
-    timerEl.textContent = time;
+    timer.textContent = time;
     // Countdown time
         interval = setInterval(function() {
             if (time > 0) {
                 time--
-                timerEl.textContent = time;
+                timer.textContent = time;
+                // Change progress bar to reflect time
+                progressBar.style.width = (time * 1.6) + "%";
             } else {
-              clearInterval(interval);
-              alert("Time has run out")
+                endQuiz();
             }
         }, 1000);
 }
@@ -183,6 +192,9 @@ function setQuestion(){
 }
 
 function correct(){
+    // Run if a correct answer is submitted
+    // Show result bar
+    result.style.opacity = "1";
     result.textContent = "Correct :)";
     result.style.backgroundColor = "#33FF5E";
     questionPosition++;
@@ -191,18 +203,89 @@ function correct(){
 }
 
 function wrong(){
-    result.textContent = "Wrong :(";
+    // Run if a wrong answer is submitted
+    // Show result bar
+    result.style.opacity = "1";
+    // Output question result and correct answer
+    result.textContent = "Wrong :( The correct answer was: " + quizObj.getCorrectAnswers(questionPosition);
     result.style.backgroundColor = "#FF5333";
+    // Reduce time
     time -= 10;
-    timerEl.textContent = time;
+    timer.textContent = time;
+    // Change progress bar to reflect time
+    progressBar.style.width = (time * 1.6) + "%";
+    // Increase question position
     questionPosition++;
     setQuestion();
 }
 
 function endQuiz(){
-    quizFrame.innerHTML = "DONE!";
+    // When quiz has ended
+    result.style.opacity = "0";
+    scoreboard.style.opacity = "0";
+    // Stop timer
     clearInterval(interval);
-    score.innerHTML = scoreValue;
+    // Create HTML output when quiz ends
+    var endHTML = "<h1>Quest completed!</h1>";
+    endHTML += "<h2>Score: " + time +"</h2>";
+    endHTML += "<p>Correct answers: " + scoreValue +" /// Incorrect answers: " + (5 - scoreValue) + "</p>";
+    endHTML += "<hr>";
+    endHTML += "<p>Please enter your initials:</p>";
+    endHTML += "<input type='text' id='initials'>";
+    endHTML += "<input type='submit' value='Submit' class='scoreSubmit'>";
+    quizFrame.innerHTML = endHTML;
+}
+
+function submitScore(){
+    scoreObj.user.push(document.querySelector('#initials').value);
+    scoreObj.score.push(time);
+    localStorage.setItem('scoreObj', JSON.stringify(scoreObj));
+    getScore();
+}
+
+function getScore(){
+    // Clear HTML Frame
+    quizFrame.innerHTML = "";
+
+    // Get score object from local storage
+    var newScoreObj = JSON.parse(localStorage.getItem('scoreObj'))
+
+    // Output high scores
+    if (scoreObj.user.length !== 0){
+    for(var i = 0; i < newScoreObj.user.length; i++){
+        var highscoreString =  newScoreObj.user[i] + " - " + newScoreObj.score[i];
+        var highscoreText = document.createElement('p');
+        highscoreText.innerHTML = highscoreString;
+        highscoreText.classList.add('highscore-text');
+        quizFrame.prepend(highscoreText);
+    }
+    // Add brackets next to the current user
+    var highScoreText = document.querySelectorAll('p')[0]
+    highScoreText.innerHTML = ">> " + highScoreText.innerHTML + " <<"
+}
+    // Create header
+    var highScoreHeader = document.createElement('h1');
+    highScoreHeader.textContent = "High Scores: ";
+    quizFrame.prepend(highScoreHeader);
+    console.log(scoreObj.user);
+
+    // Create button to replay game
+    var replayLink = document.createElement('a');
+    replayLink.textContent = "Replay";
+    replayLink.setAttribute("href","#");
+    replayLink.classList.add('replay-link');
+    replayLink.classList.add('btn');
+    quizFrame.append(replayLink);
+
+    // If the score Object has data in it, give option to clear local storage
+    if (scoreObj.user.length !== 0){
+        var clearLink = document.createElement('a');
+        clearLink.textContent = "Clear Scores";
+        clearLink.setAttribute("href","#");
+        clearLink.classList.add('clear-link');
+        clearLink.classList.add('btn');
+        quizFrame.append(clearLink);
+    }
 }
 
 // Start quiz button
@@ -214,13 +297,33 @@ startButton.addEventListener("click", function(){
 
 quizFrame.addEventListener("click", function(e){
     event.preventDefault();
-    // Show result bar
-    result.style.opacity = "1";
     // Test if the clicked answer matches the correct answer
     if (e.target.classList.value.indexOf('answer-option') !== -1 && e.target.innerHTML === quizObj.getCorrectAnswers(questionPosition)){
         correct();
     } else if (e.target.classList.value.indexOf('answer-option') > 0){
         wrong();
     }
+    
+    // Submit score button click
+    if(e.target.classList.value.indexOf('scoreSubmit') !== -1){
+        submitScore();
+    }
+    
+    // Clear scores button click
+    if(e.target.classList.value.indexOf('clear-link') !== -1){
+        scoreObj.user = [];
+        scoreObj.score = [];
+        localStorage.setItem('scoreObj', JSON.stringify(scoreObj));
+        getScore();
+    }
+
+    // Replay button game
+    if(e.target.classList.value.indexOf('replay-link') !== -1){
+        window.location.reload();
+    }
 })
 
+highscore.addEventListener("click",function(){
+    clearInterval(interval);
+    getScore();
+})
